13dc13fa15b79d1bdf0373254d7aa432d3c76353
Alex Ruiz Estradera <ahharu@ahharu.com>
2016-09-07 16:57:48 +0200

    upd

--- deployment_scripts/install_midonet_puppet_modules.sh
+++ deployment_scripts/install_midonet_puppet_modules.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
+set -e
+set -x
 
 puppet module install puppetlabs-java --version=1.6.0 --ignore-dependencies --force
 puppet module install locp-cassandra --version=1.25.2 --ignore-dependencies --force
 puppet module install puppetlabs-inifile --version=1.5.0 --ignore-dependencies --force
 puppet module install puppetlabs-apt --version=2.2.2 --ignore-dependencies --force
-puppet module install puppetlabs-java --version=1.6.0 --ignore-dependencies --force
 puppet module install puppetlabs-stdlib --version=4.12.0 --ignore-dependencies --force
 puppet module install puppetlabs-apache --version=1.10.0 --ignore-dependencies --force
-puppet module install puppetlabs-java --version=1.8.1 --ignore-dependencies --force
 puppet module install puppetlabs-concat --version=2.2.0 --ignore-dependencies --force
 puppet module install TubeMogul-curator --version=1.0.1 --ignore-dependencies --force
 puppet module install elasticsearch-elasticsearch --version=0.13.2 --ignore-dependencies --force
 puppet module install elasticsearch-logstash --version=0.6.4 --ignore-dependencies --force
 puppet module install electrical-file_concat --version=1.0.1 --ignore-dependencies --force
 puppet module install richardc-datacat --version=0.6.2 --ignore-dependencies --force
+
 
 #install git
 apt-get install -y git
