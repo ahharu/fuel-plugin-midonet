7053bfd4eff8ae21bdec5702aa4c9b70c24abe12
Alex Ruiz Estradera <ahharu@ahharu.com>
2016-09-08 17:42:20 +0200

    upd

--- deployment_scripts/install_midonet_common_modules_and_gems.sh
+++ deployment_scripts/install_midonet_common_modules_and_gems.sh
@@ -2,6 +2,7 @@
 
 puppet module install puppetlabs-java --version=1.6.0 --ignore-dependencies --force
 puppet module install locp-cassandra --version=1.25.2 --ignore-dependencies --force
+puppet module install deric-zookeeper --version=0.6.1 --ignore-dependencies --force
 puppet module install TubeMogul-curator --version=1.0.1 --ignore-dependencies --force
 puppet module install elasticsearch-elasticsearch --version=0.13.2 --ignore-dependencies --force
 puppet module install elasticsearch-logstash --version=0.6.4 --ignore-dependencies --force
