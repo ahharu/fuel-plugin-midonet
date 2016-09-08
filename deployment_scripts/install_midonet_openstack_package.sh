56555b017df46737d0ed3686fd542c16431db8bf
Alex Ruiz Estradera <ahharu@ahharu.com>
2016-09-08 12:59:59 +0200

    upd

--- deployment_scripts/install_midonet_openstack_package.sh
+++ deployment_scripts/install_midonet_openstack_package.sh
@@ -5,7 +5,7 @@ apt-get install -y git
 
 cd /tmp
 git clone https://github.com/midonet/puppet-midonet_openstack.git
-cd puppet-neutron
+cd puppet-midonet_openstack
 git fetch
 puppet module build
 puppet module install $(find . | grep .tar.gz) --ignore-dependencies --force
